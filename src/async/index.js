87ece511af22a3ea417386e87d277b3a62b49773
navnelson7 <navnelson7@gmail.com>
2021-11-16 10:17:06 -0600

    ejemplo practico usando async y await

--- src/async/index.js
+++ src/async/index.js
@@ -14,3 +14,16 @@ const doSomeThing = async () => {
 console.log('Before');
 doSomeThing();
 console.log('After');
+
+const anotherfunction = async () => {
+    try{
+        const something = await doSomethingAsync();
+        console.log(something);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+console.log('Before 1');
+anotherfunction();
+console.log('After 2');
